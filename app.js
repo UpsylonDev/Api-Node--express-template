9ca4a3d2b5b83fbb2d3b3e9c5d80a1a8e18bdf59
Uspy <upsylon.dev@gmail.com>
2019-09-06 07:58:04 +0200

    Retourner du Json

--- app.js
+++ app.js
@@ -12,19 +12,20 @@ var error = 'error'
 
 //EXPRESS get
 app.get("/api/v1/members/:id", (req, res) => {
-  res.send(members[req.params.id - 1].name);
+    res.json(members[req.params.id - 1].name);
 });
+
 
 // requete parametrée
 app.get('/api/v1/members', (req, res) => {
-    if (req.query.max != undefined &&  req.query.max > 0) {
-        res.send(members.slice(0, req.query.max))
-    } else { 
-        res.send('Selectionnez au moins un membre max')
+    if (req.query.max != undefined && req.query.max > 0) {
+        res.json(members.slice(0, req.query.max))
+    } else {
+        res.json('Selectionnez au moins un membre max')
     }
- });
+});
 
 app.listen(8080, () => {
-  // possible de mettre un callback
-  console.log("Start on port : 8080");
+    // possible de mettre un callback
+    console.log("Start on port : 8080");
 });
