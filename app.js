60dcaf8616abe4f467e6864572838ec106712763
Uspy <upsylon.dev@gmail.com>
2019-09-06 08:59:21 +0200

    Gestio ndes erreurs

--- app.js
+++ app.js
@@ -5,27 +5,40 @@ const members = require("./assets/Members");
 
 const app = express();
 
-// le smidlewares
+// les midlewares
 
 app.use(morgan("dev"))
 var error = 'error'
 
 //EXPRESS get
-app.get("/api/v1/members/:id", (req, res) => {
-    res.json(members[req.params.id - 1].name);
-});
-
+// app.get("/api/v1/members/:id", (req, res) => {
+//     res.json(success(members[req.params.id - 1].name))
+// });
 
 // requete parametrée
 app.get('/api/v1/members', (req, res) => {
     if (req.query.max != undefined && req.query.max > 0) {
-        res.json(members.slice(0, req.query.max))
-    } else {
-        res.json('Selectionnez au moins un membre max')
+        res.json(success(members.slice(0, req.query.max)))
+    } else if(req.query.max != undefined) {
+      res.json(res.json(errors('mauvaise valeur') ))
+    }
+    else {
+      // sinon retourne tous les membres si suppérieur au max
+      res.json(success(members))
     }
 });
+
 
 app.listen(8080, () => {
     // possible de mettre un callback
     console.log("Start on port : 8080");
 });
+
+// retourner le status des réponses
+function success(result) {
+  return {status: 'success', result: result}
+}
+function errors(message) {
+  return {status: 'error', result: message}
+}
+
