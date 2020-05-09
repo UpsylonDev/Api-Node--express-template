abf6d355851bc3a5b0633f5d060bc7f9a537bcc1
Yann Mingant <yann.mingant@gmail.com>
2020-05-09 10:50:34 +0200

    up

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
+  
+app.use(express.static('statics', options))
+
 
 // route de base 
 app.get('/', (req, res) => {
 
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
-
 
 app.listen(3000, () => {
     console.log('listening on *:3000 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
